000f8fa401f49e9a33740c777d92786900a109cf
ArtemSeliverstov <artem2030@gmail.com>
2026-03-19 21:50:42 +0300

    v20260319-s65: +30 Everyday English + mobile UX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='eq-v20260319-s62';
+const CACHE='eq-v20260319-s65';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.open(CACHE).then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(nr=>{c.put(e.request,nr.clone());return nr}))))});
