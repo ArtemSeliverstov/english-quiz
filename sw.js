823d5b3c8b10f80fc5cad626295836f8a48081f7
ArtemSeliverstov <artem2030@gmail.com>
2026-03-23 08:25:12 +0300

    v20260322-s70r7: remove redundant badge shelf, clean header subtitle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='grammar-quiz-v20260322-s70r5';
+const CACHE='grammar-quiz-v20260322-s70r7';
 self.addEventListener('install',e=>{self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{});
