021e0c2800584d22e77c0e740a4ebd49c179e24a
ArtemSeliverstov <artem2030@gmail.com>
2026-03-24 09:11:23 +0300

    v20260323-s71 Exercises tab + deeplink save

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='grammar-quiz-v20260322-s70r8';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{});
+const cacheName='eq-v20260323-s71';
+const assets=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(assets)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==cacheName).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
