7dc36eb7c6385a5013212d4300c834002324a84d
ArtemSeliverstov <artem2030@gmail.com>
2026-03-24 10:05:44 +0300

    v20260324-s72 Fix exercises Firebase path (players/ prefix)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName='eq-v20260323-s71';
+const cacheName='eq-v20260324-s72';
 const assets=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(assets)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==cacheName).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
