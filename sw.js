936a88bbedd9a9c5af0137528ef25ecf0bb03350
ArtemSeliverstov <artem2030@gmail.com>
2026-04-05 08:42:20 +0300

    v20260405-s83: 7 PV scaffold gap questions (pv_s01-s07)

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE='quiz-v20260404-s82';
-const URLS=['./','./index.html'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).then(()=>self.skipWaiting()))});
+const CACHE='eq-v20260405-s83';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/english-quiz/'])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const url=new URL(e.request.url);if(url.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('fetch',e=>{if(e.request.url.startsWith(self.location.origin))e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
